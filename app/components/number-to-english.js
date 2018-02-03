38daae5952142b20f2f454c28a4c318a31ca76c3
Ricardo Mercado <ricardoamercado@gmail.com>
2018-02-03 15:28:52 -0400

    Publishing to github pages

--- app/components/number-to-english.js
+++
@@ -1,1 +0,0 @@
-export { default } from 'ember-number-to-words/components/number-to-english';
--- tests/dummy/app/controllers/application.js
+++
@@ -1,7 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Controller.extend({
-  numberDefault: 10,
-  numberDecimalWord: 1.15,
-  numberDecimalFraction: 1.15
-});
