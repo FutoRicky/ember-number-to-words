39b333bca11d16106c162fed503864a6ab96c20b
Ricardo Mercado <ricardoamercado@gmail.com>
2018-04-04 14:12:02 -0400

    FIX Failing CI

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,3 +1,4 @@
+/*jshint node:true*/
 module.exports = {
   useYarn: true,
   scenarios: [
--- testem.js
+++ testem.js
@@ -1,21 +1,13 @@
+/*jshint node:true*/
 module.exports = {
-  test_page: 'tests/index.html?hidepassed',
-  disable_watching: true,
-  launch_in_ci: [
-    'Chrome'
+  "framework": "qunit",
+  "test_page": "tests/index.html?hidepassed",
+  "disable_watching": true,
+  "launch_in_ci": [
+    "PhantomJS"
   ],
-  launch_in_dev: [
-    'Chrome'
-  ],
-  browser_args: {
-    Chrome: {
-      mode: 'ci',
-      args: [
-        '--disable-gpu',
-        '--headless',
-        '--remote-debugging-port=0',
-        '--window-size=1440,900'
-      ]
-    }
-  }
+  "launch_in_dev": [
+    "PhantomJS",
+    "Chrome"
+  ]
 };
